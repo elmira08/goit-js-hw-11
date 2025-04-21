6ea516a7edba18d3ba312cf906c89289aa5b85d7
Elmira <xzrklf@gmail.com>
2025-04-21 22:37:57 +0300

    add index file

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = '49857160-7bda86bfd7838e1783d5a5b92';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data.hits; 
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
